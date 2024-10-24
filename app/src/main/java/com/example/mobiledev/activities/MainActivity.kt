bfa07c84a89af5f7e11b286a4e97b1023f97c3cf
dimapartov <mr_kubarev@mail.ru>
2024-10-24 04:30:25 +0300

    Hotfix

--- app/src/main/java/com/example/mobiledev/activities/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev/activities/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            navigateToLoginRegister()
+            navigateToOnboard()
         }
     }
 
