4789ba36fba9a9c97bcc0db1f7b8c853570cce0b
dimapartov <mr_kubarev@mail.ru>
2024-12-10 08:01:20 +0300

    Hotfix

--- app/src/main/java/com/example/mobiledev/fragments/CharactersFragment.kt
+++ app/src/main/java/com/example/mobiledev/fragments/CharactersFragment.kt
@@ -8,9 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mobiledev.databinding.FragmentCharactersBinding
 import com.example.mobiledev.api.CharacterViewModel
-import com.example.mobiledev.fragments.CharacterAdapter
+import com.example.mobiledev.databinding.FragmentCharactersBinding
 
 class CharactersFragment : Fragment() {
 
